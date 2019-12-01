d7d6cd169d6ed2e899d0ef7c2208ae9807152724
Grigorii <g.bykovskii@spb.mcart.ru>
2019-12-02 02:31:50 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/backpack_cipher.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backpack_cipher.dir/main.c.obj"
+  "backpack_cipher.exe"
+  "backpack_cipher.exe.manifest"
+  "backpack_cipher.pdb"
+  "libbackpack_cipher.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/backpack_cipher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
